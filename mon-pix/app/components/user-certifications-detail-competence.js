cc3024c229f941af31ad49b96904b4a8b1b11817
Krysthalia <ang.schall@gmail.com>
2020-06-24 12:14:13 +0200

    Add detail-competence component

--- mon-pix/app/components/user-certifications-detail-competence.js
+++ mon-pix/app/components/user-certifications-detail-competence.js
@@ -1,10 +1,15 @@
-import { classNames } from '@ember-decorators/component';
+import { computed } from '@ember/object';
 import Component from '@ember/component';
 import classic from 'ember-classic-decorator';
 
 @classic
-@classNames('user-certifications-detail-competence')
 export default class UserCertificationsDetailCompetence extends Component {
-  competence = null;
-  levels = [1,2,3,4,5,6,7,8];
+  @computed('area.resultCompetences.[]')
+  get sortedCompetences() {
+    return this.area.resultCompetences.sortBy('index');
+  }
+
+  competenceNotTested(competence) {
+    return competence.level < 1;
+  }
 }
--- mon-pix/tests/integration/components/user-certifications-detail-competence-test.js
+++ mon-pix/tests/integration/components/user-certifications-detail-competence-test.js
@@ -1,152 +1,27 @@
-import EmberObject from '@ember/object';
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { setupRenderingTest } from 'ember-mocha';
-import { find, findAll, render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
 
-describe('Integration | Component | user certifications detail competence', function() {
+describe('Integration | Component | user-certifications-detail-competence', function() {
   setupRenderingTest();
 
-  let competence;
-
-  beforeEach(function() {
-    competence = EmberObject.create({
-      'index': 1.2,
-      'level': -1,
-      'name': 'Mener une recherche et une veille d’information',
-      'score': 0,
-    });
-  });
-
   it('renders', async function() {
-    this.set('competence', competence);
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{user-certifications-detail-competence competence=competence}}`);
-    expect(find('.user-certifications-detail-competence')).to.exist;
-  });
+    await render(hbs`<UserCertificationsDetailCompetence />`);
 
-  context('when competence has level -1', function() {
+    expect(this.element.textContent.trim()).to.equal('');
 
-    beforeEach(async function() {
-      // given
-      this.set('competence', competence);
+    // Template block usage:
+    await render(hbs`
+      <UserCertificationsDetailCompetence>
+        template block text
+      </UserCertificationsDetailCompetence>
+    `);
 
-      // when
-      await render(hbs`{{user-certifications-detail-competence competence=competence}}`);
-    });
-
-    // then
-    it('should show the name of competence', function() {
-      // given
-      const divOfName = '.user-certifications-detail-competence__box-name';
-
-      // then
-      expect(find(divOfName).textContent).to.include(competence.name);
-    });
-
-    it('should not show the level of competence', function() {
-      // given
-      const divOfLevel = '.user-certifications-detail-competence__box-level';
-
-      // then
-      expect(find(divOfLevel)).to.not.exist;
-    });
-
-    it('should show all level bar in grey', function() {
-      // given
-      const divOfBarUnvalidatedLevel = '.user-certifications-detail-competence__not-validate-level';
-
-      // then
-      expect(findAll(divOfBarUnvalidatedLevel)).to.have.lengthOf(8);
-    });
-  });
-
-  context('when competence has level 0', function() {
-
-    beforeEach(async function() {
-      // given
-      competence = EmberObject.create({
-        'index': 1.2,
-        'level': 0,
-        'name': 'Mener une recherche et une veille d’information',
-        'score': 0,
-      });
-      this.set('competence', competence);
-
-      // when
-      await render(hbs`{{user-certifications-detail-competence competence=competence}}`);
-    });
-
-    // then
-    it('should show the name of competence', function() {
-      // given
-      const divOfName = '.user-certifications-detail-competence__box-name';
-
-      // then
-      expect(find(divOfName).textContent).to.include(competence.name);
-    });
-
-    it('should not show the level of competence', function() {
-      // given
-      const divOfLevel = '.user-certifications-detail-competence__box-level';
-
-      // then
-      expect(find(divOfLevel)).to.not.exist;
-    });
-
-    it('should show all level bar in grey', function() {
-      // given
-      const divOfBarUnvalidatedLevel = '.user-certifications-detail-competence__not-validate-level';
-
-      // then
-      expect(findAll(divOfBarUnvalidatedLevel)).to.have.lengthOf(8);
-    });
-  });
-
-  context('when competence has level 5', function() {
-
-    beforeEach(async function() {
-      // given
-      competence = EmberObject.create({
-        'index': 1.2,
-        'level': 5,
-        'name': 'Mener une recherche et une veille d’information',
-        'score': 41,
-      });
-      this.set('competence', competence);
-
-      // when
-      await render(hbs`{{user-certifications-detail-competence competence=competence}}`);
-    });
-
-    // then
-    it('should show the name of competence', function() {
-      // given
-      const divOfName = '.user-certifications-detail-competence__box-name';
-
-      // then
-      expect(find(divOfName).textContent).to.include(competence.name);
-    });
-
-    it('should show the level of competence', function() {
-      // given
-      const divOfLevel = '.user-certifications-detail-competence__box-level';
-
-      // then
-      expect(find(divOfLevel)).to.exist;
-      expect(find(divOfLevel).textContent).to.include(competence.level);
-
-    });
-
-    it('should show 5 level bar in yellow and 3 in grey', function() {
-      // given
-      const divOfBarValidatedLevel = '.user-certifications-detail-competence__validate-level';
-      const divOfBarUnvalidatedLevel = '.user-certifications-detail-competence__not-validate-level';
-
-      // then
-      expect(findAll(divOfBarValidatedLevel)).to.have.lengthOf(5);
-      expect(findAll(divOfBarUnvalidatedLevel)).to.have.lengthOf(3);
-    });
+    expect(this.element.textContent.trim()).to.equal('template block text');
   });
 });
